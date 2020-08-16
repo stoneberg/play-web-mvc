aeaa7d5f34e45c5664cd1e27b17657e10f43f39f
Lee YuPyeong <stoneberg73@gmail.com>
2020-08-17 03:46:34 +0900

    crop 간소화

--- src/main/resources/static/js/pages/crop/crop.js
+++ src/main/resources/static/js/pages/crop/crop.js
@@ -8,38 +8,20 @@ const module = (function (global, $, _, moment, thisPage) {
     /***************************************************************************
      * @ 모듈 함수 선언
      **************************************************************************/
-    function dataURItoBlob(dataURI) {
-        // convert base64/URLEncoded data component to raw binary data held in a string
-        let byteString;
-        if (dataURI.split(',')[0].indexOf('base64') >= 0) {
-            byteString = atob(dataURI.split(',')[1]);
-        } else {
-            byteString = unescape(dataURI.split(',')[1]);
-        }
 
-        // separate out the mime component
-        const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-        // write the bytes of the string to a typed array
-        const ia = new Uint8Array(byteString.length);
-        for (let i = 0; i < byteString.length; i++) {
-            ia[i] = byteString.charCodeAt(i);
-        }
-
-        return new Blob([ia], {type: mimeString});
-    }
 
     /***************************************************************************
      * @ jquery 이벤트 등록
      **************************************************************************/
     function moduleEventHandlers() {
 
-        let $image_crop = $('#targetImage').croppie({
-            enableExif: true,
+        // config croppie
+        let $cropImage = $('#cropImage').croppie({
+            showZoomer: false,
             viewport: {
                 width: 200,
                 height: 200,
-                type: 'square' //circle
+                type: 'square'
             },
             boundary: {
                 width: 300,
@@ -47,35 +29,39 @@ const module = (function (global, $, _, moment, thisPage) {
             }
         });
 
+        // load image on preview modal
         $('#selectImage').on('change', function () {
             const reader = new FileReader();
             reader.onload = function (event) {
-                $image_crop.croppie('bind', {
+                $cropImage.croppie('bind', {
                     url: event.target.result
-                }).then(function () {
-                    console.log('jQuery bind complete');
                 });
             }
             reader.readAsDataURL(this.files[0]);
-            $('#uploadImageModal').modal('show');
+            $('#previewModal').modal('show');
         });
 
+        // upload cropped image
         $('#uploadImage').on('click', function (e) {
-            $image_crop.croppie('result', {
-                type: 'canvas',
+            $cropImage.croppie('result', {
+                type: 'blob',
                 size: 'viewport'
-            }).then(function (dataURL) {
-                let blob = dataURItoBlob(dataURL);
+            }).then(function (blob) {
                 const formData = new FormData();
                 formData.append("file", blob);
                 formData.append("userId", "123456789");
                 formData.append("filename", $('#selectImage')[0].files[0].name);
-                $.ajaxUpload($.reqPost(CTX + "crop/upload").setData(formData).build()).done(function (response) {
+                $.ajaxUpload($.reqPost(CTX + "crop/upload")
+                    .setData(formData).build()).done(function (response) {
                     console.log("response===>", response);
                     if (response === "OK") {
                         window.alert("지정한 이미지가 저장되었습니다.");
-                        $('#uploadImageModal').modal('hide');
+                        $('#previewModal').modal('hide');
                         $('#selectImage').val('');
+                        const urlCreator = window.URL || window.webkitURL;
+                        const imageUrl = urlCreator.createObjectURL(blob);
+                        $("#croppedImage").attr("src", imageUrl);
+                        $("#saveImage").show();
                     }
                 });
             });
@@ -83,7 +69,7 @@ const module = (function (global, $, _, moment, thisPage) {
 
         $('.close-modal').on('click', function (e) {
             e.preventDefault();
-            $('#uploadImageModal').modal('hide');
+            $('#previewModal').modal('hide');
             $('#selectImage').val('');
         });
 
